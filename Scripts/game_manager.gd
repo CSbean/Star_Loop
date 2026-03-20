eff27a52d020b38cfcc51e7050f00c448a6bb22e
CSbean <2261060@flourbluffschools.org>
2026-03-20 12:41:11 -0500

    added the pistol and started its animations

---
+++ Scripts/game_manager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var paused :bool = false
+
+#credits
+# Astronaut by Quaternius
+# Scifi Pistol by Quaternius
+#
+#
+#
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,9 @@
 extends CharacterBody3D
+class_name Player
 
 @onready var camera_3d: Camera3D = $Camera3D
 @onready var animation_player: AnimationPlayer = $PlayerSprite/AnimationPlayer
+@onready var scifi_pistol: Node3D = $PlayerSprite/RootNode/CharacterArmature/Skeleton3D/BoneAttachment3D/ScifiPistol
 
 
 var SPEED = 5.0
@@ -13,6 +15,7 @@ var camra_sense := 50
 var capMouse := false
 
 var sprinting_toggle = false
+var pistolVisabilityToggle = false
 
 func _ready() -> void:
 	change_mouse()
@@ -20,45 +23,82 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("esc"):
 		change_mouse()
-	
-	if Input.is_action_just_pressed("Player_Sprint"):
-		sprinting_toggle = !sprinting_toggle
-		SPEED = 10
-	
-	if sprinting_toggle and Input.is_action_pressed("move_forward"):
-		animation_player.play("CharacterArmature|Run")
+		GameManager.paused = !GameManager.paused
+
+
+	if GameManager.paused == false:
+		if Input.is_action_just_pressed("Player_Sprint"):
+			sprinting_toggle = !sprinting_toggle
+			if sprinting_toggle:
+				SPEED = 10.0
+			else:
+				SPEED = 5.0
+			
+		#work on the gun
+		if animation_player.current_animation == "CharacterArmature|Gun_Shoot" or animation_player.current_animation == "CharacterArmature|Gun_Shoot":
+			pistolVisabilityToggle == true
+			
+		else:
+			pistolVisabilityToggle == false
 		
-	elif sprinting_toggle == false and Input.is_action_pressed("move_forward"):
-		animation_player.play("CharacterArmature|Walk")
-	else:
-		animation_player.play("CharacterArmature|Idle")
-	
-	
-	
+		
+		
+		# animations
+		if sprinting_toggle and Input.is_action_pressed("shoot"):
+			animation_player.play("CharacterArmature|Run_Shoot")
+		elif sprinting_toggle == false and Input.is_action_pressed("shoot"):
+			animation_player.play("CharacterArmature|Gun_Shoot")
+		
+		
+		#walking foward
+		elif sprinting_toggle and Input.is_action_pressed("move_forward"):
+			animation_player.play("CharacterArmature|Run")
+		elif sprinting_toggle == false and Input.is_action_pressed("move_forward"):
+			animation_player.play("CharacterArmature|Walk")
+		#walking backwards
+		elif Input.is_action_pressed("move_back"):
+			animation_player.play("CharacterArmature|Run_Back")
+		#walking right
+		elif Input.is_action_pressed("move_right"):
+			animation_player.play("CharacterArmature|Run_Right")
+		#walking left
+		elif Input.is_action_pressed("move_left"):
+			animation_player.play("CharacterArmature|Run_Left")
+			
+		#if Input.is_action_pressed("shoot"):
+		#	animation_player.play("CharacterArmature|Gun_Shoot")
+		
+		
+		else:
+			animation_player.play("CharacterArmature|Idle")
+		
+		
+		
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if GameManager.paused == false:
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 
 
-	rotate_camrea(delta)
-	move_and_slide()
+		rotate_camrea(delta)
+		move_and_slide()
 #for mouse move
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion: 
