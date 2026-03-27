ab83054623875dcd37d10e9a546ef62272472a82
CSbean <2261060@flourbluffschools.org>
2026-03-27 12:11:28 -0500

    imported stuff

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -7,6 +7,11 @@ var paused :bool = false
 # Scifi Pistol by Quaternius
 #Sci-Fi Shotgun by burunduk [CC-BY] via Poly Pizza
 #Alien by Quaternius
+#Cockpit control center by Poly by Google [CC-BY]
+#Ceiling Light by Ryan Donaldson [CC-BY]
+#
+#
+#
 #
 
 func update_health()->void:
