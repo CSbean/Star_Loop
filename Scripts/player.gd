9a67e38a9ef9ad99e09eb1d967c842cde3615b8a
CSbean <2261060@flourbluffschools.org>
2026-04-20 12:41:51 -0500

    added gun and walking audio

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,6 +8,12 @@ class_name Player
 @onready var ray_cast_3d: RayCast3D = $Camera3D/RayCast3D
 @onready var spot_light_3d: SpotLight3D = $Camera3D/SpotLight3D
 @onready var animation_tree: AnimationTree = $AnimationTree
+#audio strem players
+@onready var walking_audio_stream: AudioStreamPlayer = $walkingAudioStream
+@onready var pisto_shoot_audio: AudioStreamPlayer = $pistoShootAudio
+@onready var shotgun_shoot_audio: AudioStreamPlayer = $shotgunShootAudio
+@onready var ar_shoot_audio: AudioStreamPlayer = $arShootAudio
+
 
 var SPEED = 5.0
 const JUMP_VELOCITY = 4.
@@ -66,30 +72,27 @@ func _process(_delta: float) -> void:
 			else:
 				SPEED = 5.0
 			
-
-
-		# animations
-		#interacting
-		if Input.is_action_pressed("interact"):
-
-			animation_player.play("CharacterArmature|Interact")
 		#shooting
+		#AR = 0, Shotgun = 1, Pistol = 2
 		if(Input.is_action_just_pressed("shoot")):
 			animation_player.play("CharacterArmature|Run_Shoot")
 			if ray_cast_3d.is_colliding():
 				if (inventorySlot == 0):
 					if (rifleRounds > 0):
 						rifleRounds -= 1
+						ar_shoot_audio.play()
 						if (ray_cast_3d.get_collider() is Enemy):
 							ray_cast_3d.get_collider().health -= 30
 				elif (inventorySlot == 1):
 					if (shotgunRounds > 0):
 						shotgunRounds -= 1
+						shotgun_shoot_audio.play()
 						if (ray_cast_3d.get_collider() is Enemy):
 							ray_cast_3d.get_collider().health -= 500/(self.global_position.distance_to(ray_cast_3d.get_collider().global_position))
 				elif (inventorySlot == 2):
 					if (pistolRounds > 0):
 						pistolRounds -= 1
+						pisto_shoot_audio.play()
 						if (ray_cast_3d.get_collider() is Enemy):
 							ray_cast_3d.get_collider().health -= 40
 			
@@ -109,9 +112,13 @@ func _physics_process(delta: float) -> void:
 		var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 		if direction:
+			if not walking_audio_stream.playing:
+				walking_audio_stream.play()
 			velocity.x = direction.x * SPEED
 			velocity.z = direction.z * SPEED
 		else:
+			if walking_audio_stream.playing:
+				walking_audio_stream.stop()
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			velocity.z = move_toward(velocity.z, 0, SPEED)
 
